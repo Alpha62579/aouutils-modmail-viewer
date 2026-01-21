e0b401e3fb7484eb38ad8d0ab9fdeb32c8a1417f
Robin. <50741530+Alpha62579@users.noreply.github.com>
2026-01-21 12:12:29 +0000

    geoblocking moment

--- preload.js
+++ preload.js
@@ -14,7 +14,7 @@ function doStuff(window) {
 
     // Load JSON from URL
     try {
-        fetch(`https://corsproxy.io/?url=${url}`).then(res => {
+        fetch(`https://api.codetabs.com/v1/proxy/?quest=${url}`).then(res => {
             try {
                 return res.json()
             } catch (error) { 
