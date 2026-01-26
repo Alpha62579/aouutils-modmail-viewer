a59e1ce259f719b917d4d3a60f90054e417d2179
Robin. <50741530+Alpha62579@users.noreply.github.com>
2026-01-27 01:00:41 +0530

    yeet loading screen

--- preload.js
+++ preload.js
@@ -22,8 +22,6 @@ function doStuff(window) {
         return
     }
 
-    document.body.innerHTML = 'Loading transcripts...'
-
     Promise.all(urls.map(url =>
         fetch(`https://api.codetabs.com/v1/proxy/?quest=${encodeURIComponent(url)}`)
             .then(res => {
