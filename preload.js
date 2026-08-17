f2155108ce04bcd382dccb8321ca632678c52694
Robin. <50741530+Alpha62579@users.noreply.github.com>
2026-08-17 22:13:18 +0530

    Use my custom domain

--- preload.js
+++ preload.js
@@ -23,7 +23,7 @@ function doStuff(window) {
     }
 
     Promise.all(urls.map(url =>
-        fetch(`https://summer-cherry-119d.robinium.workers.dev/?url=${encodeURIComponent(url)}`)
+        fetch(`https://cors.robinium.dpdns.org/?url=${encodeURIComponent(url)}`)
             .then(res => {
                 if (!res.ok) {
                     throw new Error(`HTTP error ${res.status}`)
